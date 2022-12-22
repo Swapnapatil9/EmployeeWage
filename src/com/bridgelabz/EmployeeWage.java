c1fcf8ecba306088103080d23ab88fb8456efa9e
“Swapnapatil9” <swapnapatil629@gmail.com>
2022-12-22 12:56:19 +0530

    added: Employee Attendance

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,6 +3,13 @@ package com.bridgelabz;
 public class EmployeeWage
 {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to employee wage ");
+        int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
+        System.out.println(employeeCheck);
+        if (employeeCheck == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
