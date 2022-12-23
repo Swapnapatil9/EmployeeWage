68ebcc6fdf66c363d999c7fc263501b1bb2b727a
“Swapnapatil9” <swapnapatil629@gmail.com>
2022-12-23 15:11:37 +0530

    Added: Daily Wage of Employee

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,14 +2,19 @@ package com.bridgelabz;
 
 public class EmployeeWage
 {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage ");
         int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
         System.out.println(employeeCheck);
+        int dailyWage=0;
         if (employeeCheck == 1) {
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
         }
+        System.out.println("Daily Wage of employee is " + dailyWage);
     }
 }
